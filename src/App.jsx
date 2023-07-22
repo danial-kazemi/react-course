cb632dc87ad087c6ac065baa7c128960278622d5
danial-kazemi <93821704+danial-kazemi@users.noreply.github.com>
2023-07-22 23:54:14 +0330

    Add Home and Test Routers

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 
+function TestRouter () {
+    return(
+      <>
+        <h2>testRouter</h2>
+        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit voluptates deserunt ratione sint laborum tempore, quisquam quis quia distinctio et, corrupti, dolor commodi tenetur reprehenderit labore! Et magni quia dolore?</p>
+      </>
+    )
+}
 
 function App() {
 
@@ -9,7 +17,8 @@ function App() {
     
     <>
       <Routes>
-          <Route path="/" element={Home} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/test/" element={<TestRouter />} />
       </Routes>  
       
     </>   
