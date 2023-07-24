f820ecad4e8a8c6489d851523f1bcc6b135edb14
danial-kazemi <93821704+danial-kazemi@users.noreply.github.com>
2023-07-24 19:05:17 +0330

    Set theme to RTL

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App'; 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-
+import './index.css';
+import { ThemeProvider } from 'react-bootstrap';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-          <App />
+      <ThemeProvider dir="rtl">
+        <App />
+      </ThemeProvider>          
     </BrowserRouter>    
   </React.StrictMode>,
 )
